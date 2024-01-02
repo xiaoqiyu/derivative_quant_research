8d316a3279031ef13eb4178b8cd13f9cb9b90072
xiaoqiyu <rpyxqi@gmail.com>
2024-01-02 14:16:09 +0800

    test stat

--- codes/research/demos/test_double_sample.py
+++ codes/research/demos/test_double_sample.py
@@ -5,3 +5,20 @@
 # @Date  : 2023/12/29
 # @Desc  :
 # @Contact : rpyxqi@gmail.com
+
+import statsmodels.api as sm
+import numpy as np
+data = sm.datasets.get_rdataset("Duncan","carData")
+y = data.data['income']
+y = np.vstack([y,y]).reshape(-1, 1)
+x= data.data['education']
+x=np.vstack([x,x]).reshape(-1,1)
+x=sm.add_constant(x)
+
+print(x.size, y.size)
+model=sm.OLS(y,x)
+ret=model.fit()
+print(ret.summary())
+
+
+
